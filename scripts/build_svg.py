b21b0a51ee59bebbde2a774291be74fe793743b5
Bryan Abraham <bryaanabraham@gmail.com>
2026-01-28 16:30:53 +0530

    Update and rename generate_svg.py to build_svg.py

---
+++ scripts/build_svg.py
@@ -0,0 +1,20 @@
+import json
+
+with open("stats.json") as f:
+    data = json.load(f)
+
+profile = data["profile"]
+
+with open("template.svg") as f:
+    svg = f.read()
+
+svg = svg.replace("{{followers}}", str(profile["followers"]))
+svg = svg.replace("{{repos}}", str(profile["public_repos"]))
+svg = svg.replace("{{stars}}", str(profile["stars_total"]))
+svg = svg.replace("{{forks}}", str(profile["forks_total"]))
+svg = svg.replace("{{contributions}}", str(profile["contributions_last_year"]))
+
+with open("stats.svg", "w") as f:
+    f.write(svg)
+
+print("SVG built from template!")
