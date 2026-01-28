00dfb80fbd13d458ec6242ccaa081474705622ab
Bryan Abraham <bryaanabraham@gmail.com>
2026-01-28 16:37:03 +0530

    Update builld_about_svg.py

--- scripts/builld_about_svg.py
+++ scripts/builld_about_svg.py
@@ -3,16 +3,12 @@ import json
 with open("stats.json") as f:
     data = json.load(f)
 
-profile = data["age"]
+age = data["age"]
 
 with open("assets/about-card.svg") as f:
     svg = f.read()
 
-svg = svg.replace("{{followers}}", str(profile["followers"]))
-svg = svg.replace("{{repos}}", str(profile["public_repos"]))
-svg = svg.replace("{{stars}}", str(profile["stars_total"]))
-svg = svg.replace("{{forks}}", str(profile["forks_total"]))
-svg = svg.replace("{{contributions}}", str(profile["contributions_last_year"]))
+svg = svg.replace("{{age}}", str(age))
 
 with open("assets/about-card.svg", "w") as f:
     f.write(svg)
